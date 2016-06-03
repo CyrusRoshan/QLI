194b998d158fe968b321089f530fcd4dd06f59eb
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-06-03 10:43:32 -0400

    Add ip address finding, basic hashing, all untested.

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bytes"
 	"crypto/md5"
+	"errors"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -22,6 +29,18 @@ type clientStruct struct {
 	songURL    **url.URL
 	songSearch *string
 }
+
+type songHolder struct {
+	selfSubmitted bool
+	fileName      string
+	fileURL       string
+	fileSearch    string
+}
+
+var songQueue []songHolder
+var hashHolder map[string]string
+
+var downloadFolder, _ = filepath.Abs("./serverSongs")
 
 func main() {
 	app := kingpin.New("qli", "queuemmand line interface to queue music between friends")
@@ -43,7 +62,7 @@ func main() {
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 	// Register user
 	case serverMode.FullCommand():
-		println(server.port)
+		startServer(server)
 
 	case clientMode.FullCommand():
 		if *client.songFile != nil {
@@ -63,6 +82,10 @@ func panicIf(err error) {
 		panic(err)
 	}
 }
+
+//*******************
+// Client code here
+//*******************
 
 func queueFile(client clientStruct) {
 	server := "http://" + (**client.serverURL).Host
@@ -88,4 +111,100 @@ func queueURL(client clientStruct) {
 
 func queueSearch(client clientStruct) {
 
+}
+
+//*******************
+// Server code here
+//*******************
+
+func startServer(server serverStruct) {
+	log.Println("Starting qli server")
+
+	ipAddr, err := serverAddress()
+	panicIf(err)
+	log.Println("Tell your friends to send requests to " + ipAddr + ":" + strconv.Itoa(*server.port))
+	hashExistingFiles()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/checkHash", checkHash).
+		Methods("POST").
+		HeadersRegexp("Content-Type", "application/(text|json)")
+	router.HandleFunc("/uploadFile", uploadFile).
+		Methods("POST").
+		HeadersRegexp("Content-Type", "application/(text|json)")
+
+	router.NotFoundHandler = http.HandlerFunc(errorPage)
+
+	portString := strconv.Itoa(*server.port)
+	http.ListenAndServe(":"+portString, router)
+}
+
+func hashExistingFiles() {
+	if _, err := os.Stat(downloadFolder); os.IsNotExist(err) {
+		err := os.Mkdir(downloadFolder, 0777)
+		panicIf(err)
+		return
+	}
+	files, _ := ioutil.ReadDir(downloadFolder)
+	for _, file := range files {
+		dat, err := ioutil.ReadFile(file.Name())
+		panicIf(err)
+		hash := md5.Sum(dat)
+		hashHolder[string(hash[:])] = file.Name()
+	}
+
+	log.Println("Hashed existing serverSongs folder contents, which was " + strconv.Itoa(len(files)) + " total songs")
+}
+
+func checkHash(w http.ResponseWriter, r *http.Request) {
+	sentHash, err := ioutil.ReadAll(r.Body)
+	panicIf(err)
+
+	var msg string
+	songName, exists := hashHolder[string(sentHash)]
+	if exists {
+		msg = ("Hash recieved for song \"" + songName + "\", no need to send full file.")
+		w.WriteHeader(200)
+	} else {
+		msg = ("Song hash not found, send full base64 encoded file.")
+		w.WriteHeader(404)
+	}
+
+	log.Println(msg + " (ip: " + clientAddress(r) + ")")
+	w.Write([]byte(msg))
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func serverAddress() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, a := range addrs {
+		if ipnet, exists := a.(*net.IPNet); exists && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", errors.New("Local IP address not found")
+}
+
+func clientAddress(r *http.Request) string {
+	reg := regexp.MustCompile(`\[.*\]`)
+	ipAddr := reg.FindString(r.RemoteAddr)
+	ipAddr = ipAddr[1 : len(ipAddr)-1]
+	if ipAddr == "::1" {
+		ipAddr = "localhost"
+	}
+	return ipAddr
+}
+
+func errorPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") // send content in JSON
+	w.Write([]byte(`{"status": "nonexistent page"}` + "\n"))
 }
